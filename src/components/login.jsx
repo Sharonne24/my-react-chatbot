743016e5c786b1a51b84560458bb2be7b8767b8d
Sharonne24 <sawuor395@gmail.com>
2024-05-06 23:56:39 +0300

    login-password

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -30,3 +30,19 @@ const Login = () => {
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
               />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
